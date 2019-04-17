27f63fd4bce05a99f22060816e2e829f5c6df6f9
LusterFlame <kurasiki.homura@gmail.com>
2019-04-17 16:01:57 +0900

    Adjusting

--- load_and_train.py
+++ load_and_train.py
@@ -1,23 +1,10 @@
 import pickle
 
-with open("ml_lvl1.pickle", "rb") as f:
-    data_list = pickle.load(f)
-with open("ml_lvl2.pickle", "rb") as f:
-    data_list += pickle.load(f)
-with open("ml_lvl3.pickle", "rb") as f:
-    data_list += pickle.load(f)
-with open("ml_lvl4.pickle", "rb") as f:
-    data_list += pickle.load(f)
-with open("ml_lvl5.pickle", "rb") as f:
-    data_list += pickle.load(f)
-with open("ml_lvl6.pickle", "rb") as f:
-    data_list += pickle.load(f)
-with open("ml_lvl7.pickle", "rb") as f:
-    data_list += pickle.load(f)
-with open("ml_lvl8.pickle", "rb") as f:
-    data_list += pickle.load(f)
-with open("ml_lvl9.pickle", "rb") as f:
-    data_list += pickle.load(f)
+data_list = []
+for temp in range (1, 16):
+    pickleFileName = "ml_lvl" + str(temp) + ".pickle"
+    with open(pickleFileName, "rb") as f:
+        data_list += pickle.load(f)
 
 
 # Saving Information
@@ -26,12 +13,13 @@ Status = []
 Ballposition = []
 PlatformPosition = []
 Bricks = []
-for i in range(0, len(data_list)):
-    Frame.append(data_list[i].frame)
-    Status.append(data_list[i].status)
-    Ballposition.append(data_list[i].ball)
-    PlatformPosition.append(data_list[i].platform)
-    # Bricks.append(data_list[i].bricks)
+for j in range(0, 10):
+    for i in range(0, len(data_list)):
+        Frame.append(data_list[i].frame)
+        Status.append(data_list[i].status)
+        Ballposition.append(data_list[i].ball)
+        PlatformPosition.append(data_list[i].platform)
+        Bricks.append(data_list[i].bricks)
 
 # Calculating Instruction for Each Frame Using Platform Position
 import numpy as np
@@ -39,46 +27,36 @@ import numpy as np
 PlatX = np.array(PlatformPosition)[:, 0][:, np.newaxis]
 PlatX_next = PlatX[1:, :]
 instruct = (PlatX_next - PlatX[0:len(PlatX_next), 0][:, np.newaxis]) / 5
+# Amplfing first few steps when moving long distance,
+for temp in range(0, len(instruct) - 10):
+    MovingAlot = True
+    for temp2 in range (1, 10):
+        if instruct[temp + temp2] != instruct[temp]:
+            MovingAlot = False
+    if MovingAlot:
+        instruct[temp] *= 2
 
 # Setting x and y
 Ballarray = np.array(Ballposition[:-1])
-x = np.hstack((Ballarray, PlatX[0:-1, 0][:, np.newaxis]))
+x = np.hstack((Ballarray, PlatX[0:-1, 0][:, np.newaxis], ))
 y = instruct
 
 # Using Random Forest
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error 
-"""
-RMSE = 1
-finalI = 0
-for i in range(0, 1000):
-"""
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
-forest_reg = RandomForestRegressor(random_state = 701)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 810)
+forest_reg = RandomForestRegressor(random_state = 114514)
 forest_reg.fit(x_train, np.ravel(y_train))
 y_predict = forest_reg.predict(x_test)
 MSE = mean_squared_error(y_test, y_predict)
 RMSE = np.sqrt(MSE)
 print(RMSE)
-"""
-    if np.sqrt(MSE) < RMSE:
-        RMSE = np.sqrt(MSE)
-        finalI = i
-print(RMSE)
-print(finalI)
-"""
-
 
 # Save Model
 import pickle
 filename = "forestTrained.sav"
 pickle.dump(forest_reg, open(filename, 'wb'))
 
-"""
-# load model
-l_model = pickle.load(open(filename,'rb'))
-yp_l = l_model.predict(x_test)
-print("acc load: %f " % accuracy_score(yp_l, y_test))
-"""
 
--- ml_play.py
+++ ml_play.py
@@ -6,7 +6,6 @@ import numpy as np
 def ml_loop():
     filename = "forestTrained.sav"
     load_model = pickle.load(open(filename, 'rb'))
-
     comm.ml_ready()
 
     while True:
@@ -20,9 +19,10 @@ def ml_loop():
         input_temp = np.array([scene_info.ball[0], scene_info.ball[1], scene_info.platform[0]])
         input = input_temp[np.newaxis, :]
         print(load_model.predict(input))
-        if load_model.predict(input) > 0:
+        print(input)
+        if load_model.predict(input) > 0.1:
             comm.send_instruction(scene_info.frame, GameInstruction.CMD_RIGHT)
-        elif load_model.predict(input) < 0:
+        elif load_model.predict(input) < -0.1:
             comm.send_instruction(scene_info.frame, GameInstruction.CMD_LEFT)
         else:
             comm.send_instruction(scene_info.frame, GameInstruction.CMD_NONE)
